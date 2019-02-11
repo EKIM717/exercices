be7f42266280d202a5a8e04522fa8b038bc7f640
EKIM <l12391835@163.com>
2019-02-11 18:04:24 +0800

    new exercices

--- exercices/src/hacker_rank/project_euler/P12_Highly_Divisible_Triangular_Number.java
+++ exercices/src/hacker_rank/project_euler/P12_Highly_Divisible_Triangular_Number.java
@@ -1,5 +1,71 @@
 package hacker_rank.project_euler;
 
+import java.util.Scanner;
+
 public class P12_Highly_Divisible_Triangular_Number {
+
+	public static void main(String[] args) {
+		int limit = 1001;
+		int[] a = new int[limit];
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		for (int a0 = 0; a0 < n; a0++) {
+			int i = in.nextInt();
+			System.out.println(getNum(a, i));
+		}
+		in.close();
+	}
+
+	private static int getNum(int[] a, int i) {
+		if (a[i] == 0) {
+			int a0 = 1;
+			int temp = 0;
+			int num = 0;
+			while (temp <= i) {
+				num = (a0 * (a0 + 1) / 2);
+				temp = numberOfDivisors(num);
+				a0++;
+			}
+			a[i] = num;
+		}
+		return a[i];
+	}
+
+	/**
+	 * hint : if the prime factors of a number is a^n , b^m , c^q
+	 * then the number of divisors is (n+1)(m+1)(q+1)
+	 * 
+	 */
+	private static int numberOfDivisors(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		if (n == 2 || n == 3) {
+			return 2;
+		}
+		int numberOfFactor = 1;
+		//the factor of 2
+		int i = 2;
+		int count = 0;
+		while (n % i == 0) {
+			n /= i;
+			count++;
+		}
+		numberOfFactor *= ++count;
+		//the factor that is greater than 2
+		for (i = 3; i * i <= n; i += 2) {
+			int tmp = 0;
+			while (n % i == 0) {
+				n /= i;
+				tmp++;
+			}
+			numberOfFactor *= ++tmp;
+		}
+		// n is a prime
+		if (n > 1)
+			return numberOfFactor * 2;
+
+		return numberOfFactor;
+	}
 
 }
---
+++ exercices/src/hacker_rank/project_euler/P13_Large_Sum.java
@@ -0,0 +1,50 @@
+package hacker_rank.project_euler;
+
+import java.util.Scanner;
+
+public class P13_Large_Sum {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int tenDigit = 10;
+		int length = 50 / tenDigit;
+		long[][] a = new long[t][length];
+		while (t-- > 0) {
+			String s = in.next();
+			long[] a0 = a[t];
+			char[] ch = s.toCharArray();
+			int j = 0;
+			for (int i = ch.length; i - 10 >= 0; i -= 10) {
+				String subStr = s.substring(i - 10, i);
+				Long tmp = Long.valueOf(subStr);
+				a0[j++] = tmp;
+			}
+		}
+		long digits = 100000_00000L;
+		int carried = 0;
+		for (int j = 0; j < a[0].length - 1; j++) {
+			long tmp = 0;
+			for (int i = 0; i < a.length; i++) {
+//				System.out.print(a[i][j] + " ");
+				tmp += a[i][j];
+			}
+//			System.out.println("sum is " + tmp + " carried is " + carried);
+			tmp += carried;
+			carried = (int) (tmp / digits);
+		}
+		long result = 0;
+		for (int i = 0; i < a.length; i++) {
+			result += a[i][a.length - 1];
+		}
+//		System.out.println("sum is " + result + " carried is " + carried);
+		result += carried;
+		System.out.println(result / 10);
+	}
+
+	private static void foo(int[][] a) {
+
+	}
+
+	// private static int
+}
