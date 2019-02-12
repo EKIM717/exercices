aa4a9e61b9ed1e823bf4367af1eb3ce0144001c2
EKIM <l12391835@163.com>
2019-02-12 11:03:10 +0800

    larger sum

--- exercices/src/hacker_rank/project_euler/P13_Large_Sum.java
+++ exercices/src/hacker_rank/project_euler/P13_Large_Sum.java
@@ -2,54 +2,57 @@ package hacker_rank.project_euler;
 
 import java.util.Scanner;
 
-import javax.tools.Diagnostic;
-
 public class P13_Large_Sum {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int t = in.nextInt();
-		int tenDigit = 10;
-		int length = 50 / tenDigit;
-		long[][] a = new long[t][length];
+		int[] c = new int[60];
+		for (int i = 0; i < c.length; i++) {
+			c[i] = 0;
+		}
 		while (t-- > 0) {
+			// the input string
 			String s = in.next();
-			long[] a0 = a[t];
 			char[] ch = s.toCharArray();
-			int j = 0;
-			for (int i = ch.length; i - 10 >= 0; i -= 10) {
-				String subStr = s.substring(i - 10, i);
-				Long tmp = Long.valueOf(subStr);
-				a0[j++] = tmp;
+			int i = 0;
+			int carry = 0;
+			// 50 digits sum
+			for (int j = ch.length - 1; j >= 0; j--, i++) {
+				int sum = c[i] + ch[j] - '0' + carry;
+				// next carry
+				carry = sum / 10;
+				c[i] = sum % 10;
+			}
+			// extra carry
+			while (i < c.length) {
+				if (carry == 0) {
+					break;
+				}
+				int sum = c[i] + carry;
+				// next carry
+				carry = sum / 10;
+				c[i++] = sum % 10;
 			}
 		}
-		long digits = 100000_00000L;
-		int carried = 0;
-		for (int j = 0; j < a[0].length - 1; j++) {
-			long tmp = 0;
-			for (int i = 0; i < a.length; i++) {
-				// System.out.print(a[i][j] + " ");
-				tmp += a[i][j];
+		// output
+		int record = 0;
+		for (int i = c.length - 1; i >= 0; i--) {
+			// find the first index of the number that is not equal to zero,
+			// from behind to front
+			if (c[i] > 0) {
+				record = i;
+				break;
 			}
-			// System.out.println("sum is " + tmp + " carried is " + carried);
-			tmp += carried;
-			carried = (int) (tmp / digits);
 		}
-		long result = 0;
-		for (int i = 0; i < a.length; i++) {
-			result += a[i][a.length - 1];
+		int i = 0;
+		StringBuilder sb = new StringBuilder();
+		// reverse
+		while (record - 9 >= 0 && i < 10) {
+			sb.append(c[record - i++]);
 		}
-		// System.out.println("sum is " + result + " carried is " + carried);
-		result += carried;
-		long digitExceed = (result / digits);
-		int tenPow = 1;
-		if (digitExceed > 0) {
-			do {
-				tenPow *= 10;
-				digitExceed /= 10L;
-			} while (digitExceed > 0L);
-		}
-		System.out.println(result / tenPow);
+		System.out.println(sb.toString());
+		in.close();
 	}
 
 }
