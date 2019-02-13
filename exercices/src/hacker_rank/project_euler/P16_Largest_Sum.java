3b72bdb76e602beaa07de424ce754c1395e4fdf4
EKIM <l12391835@163.com>
2019-02-13 14:30:42 +0800

    P16 Power digit sum

--- exercices/src/hacker_rank/project_euler/P16_Largest_Sum.java
+++
@@ -1,5 +0,0 @@
-package hacker_rank.project_euler;
-
-public class P16_Largest_Sum {
-
-}
---
+++ exercices/src/hacker_rank/project_euler/P16_Power_Ddigit_Sum.java
@@ -0,0 +1,20 @@
+package hacker_rank.project_euler;
+
+import java.util.Scanner;
+
+public class P16_Power_Ddigit_Sum {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+			int a0 = in.nextInt();
+			long a = foo(a0);
+		}
+	}
+
+	private static long foo(int a0) {
+		
+		return 0;
+	}
+}
