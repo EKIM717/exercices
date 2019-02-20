eb526ce7309ce8c3a36c97ebb10349e08a327b45
EKIM <l12391835@163.com>
2019-02-20 16:27:41 +0800

    P25 N-digit Fibonacci number

--- exercices/src/hacker_rank/T.java
+++ exercices/src/hacker_rank/T.java
@@ -1,65 +1,92 @@
 package hacker_rank;
 
-import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class T {
 
 	public static void main(String[] args) {
-//		long a = 1;
-//		for (int i = 500; i > 250; i--) {
-//			a *= i;
-//			System.out.println(i + " " + a);
-//		}
-		System.out.println(LocalDateTime.now());
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		while (t-- > 0) {
+			int a0 = in.nextInt();
+			if (lastBitNum <= a0) {
+				calc(a0);
+			}
+			System.out.println(result[a0]);
+		}
+		in.close();
 	}
-	
-	static void foo() {
-		#define MAX 3500
-		char num_arr[10001][MAX];
-		long int sum_arr[10001] = {0};
-		void power(char num_arr[][MAX], int i){
-		    int p = i-1;
-		    char take_over = 0;
-		    char temp = 0;
-		    for(int j = MAX-1; j >=0; j--){
-		        temp = (num_arr[p][j] * 2) + take_over;
-		        if(temp >= 10){
-		            take_over = temp/10;
-		            num_arr[i][j] = temp%10;
-		        }
-		        else{
-		            num_arr[i][j] = temp;
-		            take_over = 0;
-		        }
-		    }
-		        
-		}
 
+	static int LENGTH = 10000;
 
-		void populate_num_arr(char num_arr[][3500]){
-		   for(int i = 2; i < 10001; i++){
-		       power(num_arr, i);
-		   } 
-		}
+	static int DIGIT_NUMBER = 5001;
 
-		void populate_sum_arr(long int sum_arr[], char num_arr[][MAX]){
-		    for(int i = 1; i < 10001; i++)
-		        for(int j = MAX-1; j>=0; j--)
-		            sum_arr[i] += num_arr[i][j];
-		}
+	static int[][] a = new int[2][DIGIT_NUMBER];
 
-		int main() {
-		    num_arr[1][MAX-1] = 2;
-		    populate_num_arr(num_arr);
-		    populate_sum_arr(sum_arr, num_arr);
-		    int t;
-		    scanf("%d", &t);
-		    while(t--){
-		        int n;
-		        scanf("%d", &n);
-		        printf("%ld\n", sum_arr[n]);
-		    }
-		    return 0;
+	static int[] result = new int[DIGIT_NUMBER];// store the result
+
+	static int lastBitNum = 1;// digit N
+
+	static int lastArrayIndex = 2;// which term
+
+	/**
+	 * initial
+	 */
+	static {
+		a[0][0] = 1;
+		a[1][0] = 1;
+	}
+
+	static void calc(int i) {
+//		System.out.println(LocalDateTime.now());
+		while (lastBitNum <= i) {
+			lastArrayIndex = find(lastBitNum, lastArrayIndex);
+//			System.out.println("lastBitNum : " + lastBitNum + " index : " + lastArrayIndex);
+			lastBitNum++;
 		}
+//		System.out.println(LocalDateTime.now());
+	}
+
+	/**
+	 * 
+	 * @param lastBitNum
+	 * @param lastArrayIndex
+	 * @return
+	 */
+	private static int find(int lastBitNum, int lastArrayIndex) {
+		int[] tmp = new int[DIGIT_NUMBER];
+		while (true) {
+			tmp = nextFibonacciNum(a[0], a[1]);
+			a[0] = a[1];
+			a[1] = tmp;
+			int bitNum = calcBit(tmp);
+			lastArrayIndex++;
+			if (bitNum == lastBitNum) {
+				result[lastBitNum] = lastArrayIndex;
+				return lastArrayIndex;
+			}
+		}
+	}
+
+	static int calcBit(int[] a) {
+		int index = 0;
+		for (int i = a.length - 1; i >= 0; i--) {
+			if (a[i] > 0) {
+				return i + 1;
+			}
+		}
+		return index;
+	}
+
+	private static int[] nextFibonacciNum(int[] a, int[] b) {
+		int[] c = new int[a.length];
+		int carry = 0;
+		for (int i = 0; i < DIGIT_NUMBER; i++) {
+			int result = (a[i] + b[i] + carry);
+			int remainder = result % 10;
+			c[i] = remainder;
+			carry = result / 10;
+		}
+		return c;
 	}
 }
