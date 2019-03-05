767d43d38c0f6288abca63c0bcc1e187b8ed73f7
EKIM <l12391835@163.com>
2019-03-05 15:22:37 +0800

    P1 modification

--- exercices/src/hacker_rank/project_euler/P1_MultiplesOf_3_and_5.java
+++ exercices/src/hacker_rank/project_euler/P1_MultiplesOf_3_and_5.java
@@ -20,49 +20,14 @@ public class P1_MultiplesOf_3_and_5 {
 	}
 
 	private static long calc(long n, long divider) {
-		long times = maxQuotient(n, divider);
-		return calcSum(1 * divider, times * divider, times);
-
-	}
-
-	/**
-	 * calculate the sum of arithmetic sequence 
-	 * @param firstItem
-	 * @param lastItem
-	 * @param times
-	 * @return
-	 */
-	private static long calcSum(long firstItem, long lastItem, long times) {
+		//get the max quotient, above or equals to zero
+		long times = n / divider;
 		if (times == 0)
 			return 0;
-		long result = (firstItem + lastItem) * times / 2;
-		return result;
+		// * calculate the sum of arithmetic sequence 
+		long result = divider * (1 + times) * times / 2;
+		return result;//calcSum(1 * divider, times * divider, times);
+
 	}
 
-	/**
-	 * get the max quotient, above or equals to zero
-	 * @param n
-	 * @param divider
-	 * @return
-	 */
-	private static long maxQuotient(long n, long divider) {
-		for (long i = n; i > 0; i--) {
-			if (i % divider == 0) {
-				return i / divider;
-			}
-		}
-		return 0;
-	}
-
-//	private static long sumOfevery15() {
-//		long resultOf3 = calc(15, 3);
-//		long resultOf5 = calc(15, 5);
-//		return resultOf3 + resultOf5 - 15;
-//	}
-//
-//	private static long numberOfDividerOf15() {
-//		long numOfDivider3 = maxQuotient(15, 3);
-//		long numOfDivider5 = maxQuotient(15, 5);
-//		return numOfDivider3 + numOfDivider5 - 1;
-//	}
 }
