d17ff41162d8f15909a8be626b7e13122c84deeb
EKIM <l12391835@163.com>
2019-03-12 19:04:11 +0800

    P29 modification

--- exercices/src/hacker_rank/project_euler/P29_from_others.java
+++ exercices/src/hacker_rank/project_euler/P29_from_others.java
@@ -1,23 +1,102 @@
 package hacker_rank.project_euler;
 
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class P29_from_others {
 
-	static int[] a = new int[100000 + 1];
 	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		Set<Double> set = new HashSet<>();
-		for (int i = 2; i < n; i++) {
-			for (int j = 2; j < n; j++) {
-				double d = Math.log(i) * j;
-				set.add(d);
-			}
-		}
-		System.out.println(set.size());
-	}
+	public static void main(String[] args) throws IOException{
+        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(f.readLine());
+        System.out.println(LocalDateTime.now());
+        Long output = (n - 1) * (n - 1);
+        
+        ArrayList<Integer> lists = new ArrayList<>();
+        boolean[] checker = new boolean[200001];
+        
+        Arrays.fill(checker, true);
+        checker[0] = false;
+        checker[1] = false;
+        
+        for(int i = 0; i < checker.length; i++){
+            if(checker[i]){
+                lists.add(i);
+                for(int j = i * i; j < checker.length && j > 0; j*=i){
+                    checker[j] = false;
+                }
+            }
+        }
+        
+        int[] prime = new int[lists.size()];
+        
+        for(int i = 0; i < prime.length; i++) prime[i] = lists.get(i);
+        
+//        System.out.println(Arrays.toString(prime));
+        
+        long num = 0;
+        long base = 1;
+            
+        long largest = prime[0];
+        long times = 1;
+        while(largest*prime[0] <= n){
+            largest *= prime[0];
+            times++;
+        }
+            
+        boolean[][] check = new boolean[(int)times + 1][(int)n + 1];
+        for(int k = 1; k < check.length; k++){
+            for(int j = 1; j < check[0].length; j++){
+                if(j != 0 && j != 1) check[k][j] = true;
+            }
+        }
+            
+        while(base*prime[0] <= n){
+            base *= prime[0];
+            num++;
+            long cden = num + 1;
+            long cbase = base;
+
+            while(cbase*prime[0] <= n){
+                cbase *= prime[0];
+                //System.out.println(": " + cbase + " " + (int)exp);
+                    
+                for(int j = 2; cden*j <= num*n; j++){
+                    if(cden * j % num == 0)check[(int)cden][j] = false;
+                }
+                cden++;
+            }
+                
+                
+        }
+        
+        long[] sol = new long[check.length];
+        
+        for(int i = 1; i < sol.length; i++){
+            int cur = 0;
+            for(int k = 1; k < check[0].length; k++){
+                if(k != 0 && k != 1 && !check[i][k]) sol[i]++;
+            }
+        }
+        
+        for(int i = 0; prime[i] <= Math.sqrt(n) + 1; i++){
+            long clargest = prime[i];
+            int ctimes = 1;
+            while(clargest*prime[i] <= n){
+                clargest *= prime[i];
+                ctimes++;
+            }
+            
+            for(int k = 1; k <= ctimes; k++){
+                output -= sol[k];
+            }
+        }
+        
+        System.out.println(output);
+        System.out.println(LocalDateTime.now());
+    }
 }
--- exercices/src/hacker_rank/project_euler/unfinished/P29.java
+++ exercices/src/hacker_rank/project_euler/unfinished/P29.java
@@ -18,24 +18,25 @@ public class P29 {
 		// store the prime
 		boolean[] prime_array = new boolean[100001];
 		// findPrime
-//		System.out.println("findPrime------------");
-//		System.out.println(LocalDateTime.now());
-//		int primeCount = 1;
+		System.out.println("findPrime------------");
+		System.out.println(LocalDateTime.now());
 		for (int i = 2; i <= limit; i++) {
 			if (isPrime(i)) {
 				prime_array[i] = true;
-//				primeCount++;
 			}
 		}
-//		System.out.println(LocalDateTime.now());
+		System.out.println(LocalDateTime.now());
 
 		Scanner in = new Scanner(System.in);
 		int a = in.nextInt();
+		System.out.println("primeFactorization------------");
+		System.out.println(LocalDateTime.now());
 		TreeMap<Integer, Map<Integer, Integer>> totalDivisor = new TreeMap<>();
 		for (int i = 2; i <= a; i++) {
 			totalDivisor.put(i, primeFactorization(i, prime_array));
 		}
-//		System.out.println("------------------------");
+		System.out.println(LocalDateTime.now());
+		System.out.println("calculation------------------------");
 		System.out.println(LocalDateTime.now());
 		System.out.println(foo(a, prime_array, totalDivisor));
 		System.out.println(LocalDateTime.now());
@@ -89,25 +90,11 @@ public class P29 {
 		int sum = 0;
 		for (int i = 2; i <= n; i++) {
 			// if i = m^p * n^q, the map is {{m: p}, {n, q}}
-			if (i == 15 * 15) {
-				System.out.println();
-			}
 			Map<Integer, Integer> map = totalDivisor.get(i);
-//	            if (null != map.get(i) && map.get(i) == 1) {// if i is a prime
-//	                sum += n - 1;
-//	                Set<Integer> set = new HashSet<>();
-//	                for (int j = 2; j <= n; j++) {
-//	                    set.add(j);
-//	                }
-////	                powMap.put(i, set);
-//	            } else {
-//			int kkk = 1;
 			StringBuilder valueStr = new StringBuilder();
-	        StringBuilder keyStr = new StringBuilder();
+			StringBuilder keyStr = new StringBuilder();
 			for (Integer k : map.keySet()) {
-//				kkk *= k;
 				keyStr.append(k + "*");
-//		            	int powNum = map.get(k) * j;
 				valueStr.append(map.get(k) + " ");
 			}
 			keyStr.deleteCharAt(keyStr.length() - 1);
@@ -118,34 +105,27 @@ public class P29 {
 			} else {
 				powMap.get(keyStr.toString()).add(valueStr.toString());
 			}
-//	                for (int j = 2; j <= n; j++) {
-////	                	totalDivisor.get(j);
-////	                    String key = keyStr.toString();
-//	                    if (!set.contains(valueStr.toString())) {
-////	                        sum++;
-////	                        set.add(valueStr.toString());
-//	                    }
-//	                }
-//	            }
 		}
-		//calc
+		// calc
 		int countOne = 0;
 		Set<String> uniqueMultiple = new HashSet<>();
 		for (String k : powMap.keySet()) {
-			if (powMap.get(k).size() == 1) {//only have one multiple below 100000,so add N - 1
+			if (powMap.get(k).size() == 1) {// only have one multiple below 100000,so add N - 1
 				uniqueMultiple.add(k);
 				countOne++;
-				System.out.println(k + "  " +  powMap.get(k));
-				sum += n - 1;
+//				System.out.println(k + "  " + powMap.get(k));
 			}
 		}
+		sum += (n - 1) * countOne;
 		System.out.println("only have one multiple   " + countOne);
+		System.out.println(LocalDateTime.now());
 		System.out.println(powMap.keySet().size());
 		System.out.println("after remove");
 		powMap.keySet().removeAll(uniqueMultiple);
 		System.out.println(powMap.keySet().size());
 		for (String k : powMap.keySet()) {
-				System.out.println(k + "  " +  powMap.get(k));
+			System.out.println(k + "  " + powMap.get(k));
+			
 		}
 //			if (null != powMap.get(k)) {
 //				for (String j : powMap.get(k))
