f700aba1fa58ade557baaeac5b835172ea694080
EKIM <l12391835@163.com>
2019-11-19 17:44:45 +0800

    unfinished

--- exercices/src/hacker_rank/interview_preparation_kit/arrays/MinimumSwaps2.java
+++ exercices/src/hacker_rank/interview_preparation_kit/arrays/MinimumSwaps2.java
@@ -7,30 +7,71 @@ import java.util.Scanner;
 
 public class MinimumSwaps2 {
 
-	 // Complete the minimumSwaps function below.
-    static int minimumSwaps(int[] arr) {
-    	int startIndex = 0;
-    	int lastIndex = 0;
-    	for (int i = startIndex; i < arr.length - 1; i++) {
-    		if (arr[i] > arr[i + 1]) {
-    			while (i + 2 < arr.length && arr[i + 1] < arr[i + 2] && arr[i + 2] <= arr[i]) {
-    				i++;
-    			}
-    		} else {
-    			startIndex = i;
-    		}
-    	}
-    	return bubbleByOneDirection(arr);
+	// Complete the minimumSwaps function below.
+	static int minimumSwaps(int[] arr) {
+		int count = 0;
+		outter: for (int m = 0; m < arr.length - 1; m++) {
+			for (int a : arr) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+			int indexStart = 0;
+			int left = arr[indexStart];
+			int right = 0;
+			inner2: for (int k = indexStart + 1; k < arr.length; k++) {
+				right = arr[k];
+				if (indexStart == k - 1) {
+					continue;
+				}
+				if (arr[k] < arr[k - 1]) {
+					if (arr[indexStart] > arr[k]) {
+						
+					}
+				} else {
+					if (k == arr.length - 1) {
+						
+					}
+				}
+				if (left <= right) {
+					continue;
+				}
+				if (left > right) {
+					// see if there are greater numbers
+					if (k < arr.length - 1) {
+						if (arr[k + 1] <= arr[k + 2]) {
+							if (k < arr.length - 3) {
+								indexStart++;
+								continue inner2;
+							} else {
+								count++;
+								swap(k, k+1, arr);
+								continue outter;
+							}
+						} else {
+							count++;
+							swap(indexStart, k+2, arr);
+							continue outter;
+						}
+					} else {
+						swap(k, k + 1, arr);
+						count++;
+						break outter;
+					}
+				}
+			}
+			indexStart++;
+		}
+		return count;
 
-    }
-    
-    static void swap(int i, int j, int[] q) {
-		int tmp = q[i];
-		q[i] = q[j];
-		q[j] = tmp;
 	}
-    
-    static int bubbleByOneDirection(int[] q) {
+
+	static void swap(int i, int j, int[] arr) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	static int bubbleByOneDirection(int[] q) {
 		int k = q.length;
 		int max = 0;
 		int recordSortedIndex = 0;
@@ -54,31 +95,31 @@ public class MinimumSwaps2 {
 		return max;
 	}
 
-    private static final Scanner scanner = new Scanner(System.in);
+	private static final Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+	public static void main(String[] args) throws IOException {
+//		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int[] arr = new int[n];
+		int[] arr = new int[n];
 
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		String[] arrItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
+		for (int i = 0; i < n; i++) {
+			int arrItem = Integer.parseInt(arrItems[i]);
+			arr[i] = arrItem;
+		}
 
-        int res = minimumSwaps(arr);
+		int res = minimumSwaps(arr);
 
-        bufferedWriter.write(String.valueOf(res));
-        bufferedWriter.newLine();
+//		bufferedWriter.write(String.valueOf(res));
+//		bufferedWriter.newLine();
+//
+//		bufferedWriter.close();
 
-        bufferedWriter.close();
-
-        scanner.close();
-    }
+		scanner.close();
+	}
 }
