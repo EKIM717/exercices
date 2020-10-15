8772f179f71608a91040091f290345106df5965f
EKIM <l12391835@163.com>
2020-10-15 10:47:27 +0800

    P26 reciprocal cycles finished.

--- exercices/src/hacker_rank/project_euler/P26_Reciprocal_cycles.java
+++ exercices/src/hacker_rank/project_euler/P26_Reciprocal_cycles.java
@@ -12,6 +12,9 @@ public class P26_Reciprocal_cycles {
 	static int record = 4;
 	
 	static {
+		for (int i = 0; i < limit; i++) {
+			result[i] = -1;
+		}
 		bar(limit);
 	}
 	
@@ -20,21 +23,25 @@ public class P26_Reciprocal_cycles {
 	 * @param n
 	 */
 	private static void bar(int n) {
+		number[0] = number[1] = number[2] = 0;
 		number[3] = 3;
+		result[0] = result[1] = result[2] = 0;
 		result[3] = 1;
+		long max = 1;
+//			System.out.println(LocalDateTime.now());
 		for (int i = record; i < n; i++) {
-			System.out.println("-----------" + i + "----------");
-			System.out.println(LocalDateTime.now());
+//			System.out.println("-----------" + i + "----------");
 			result[i] = foo(i);
-			System.out.println(LocalDateTime.now());
-			if (result[i] <= result[i - 1]) {
+			if (result[i] <= max) {
 				number[i] = number[i - 1];
-				result[i] = result[i - 1];
 			} else {
 				number[i] = i;
-				System.out.println("number    " + i);
+				max = result[i];
 			}
+//			System.out.println("number\t" + number[i]);
+//			System.out.println("result\t" + result[i]);
 		}
+//			System.out.println(LocalDateTime.now());
 	}
 
 	public static void main(String[] args) {
@@ -42,11 +49,8 @@ public class P26_Reciprocal_cycles {
 		int t = in.nextInt();
 		while (t-- > 0) {
 			int i = in.nextInt();
-//			if (i >= record) {
-//				bar(i);
-//				record = i + 1;
-//			}
-			if (number[i] == i) {//d < N
+			//d < N
+			if (number[i] == i) {
 				System.out.println(number[i - 1]);
 			} else {
 				System.out.println(number[i]);
@@ -54,71 +58,42 @@ public class P26_Reciprocal_cycles {
 		}
 		in.close();
 	}
+	
+	private static int dividable(int temp, int x) {
+		while (temp != 0 && temp % x == 0) {
+			temp /= x;
+		}
+		if (temp == 1 || temp == 0) {
+			return 0;
+		}
+		return temp;
+	}
 
 	private static long foo(int n) {
-		int bits = getDividerBit(n);
-		int dividend = (int) Math.pow(10, bits);
-		//
-		if ((dividend - 1) % n == 0) {
-			return bits;
-		}
-		if (check(n)) {
+		// check divided by 2
+		n = dividable(n, 2);
+		// check divided by 5
+		n = dividable(n, 5);
+		if (n == 0) {
 			return 0L;
+		}
+		// check if exits
+		if (result[n] > -1) {
+			return result[n];
 		}
 		long index = 0;
 //		System.out.println(n);
-		List<Integer> list = new ArrayList<>();
+		int dividend = 10;
 		do {
-			int remainder = dividend % n;
-			System.out.print(dividend);
-			if (0 == remainder) {
-				return 0L;
+			dividend %= n;
+			index ++;
+//			System.out.print(dividend + " ");
+			if (1 == dividend) {
+				return index;
+			} else {
+				dividend *= 10;
 			}
-			if (list.contains(remainder)) {
-				break;
-			}
-			list.add(remainder);
-			int digit = 1;
-			while ((remainder *= 10) < n) {
-				digit++;
-			}
-			index += digit;
-			dividend = remainder;
 		} while (true);
-		System.out.println();
-		System.out.println("    " + index + "    ");
-		System.out.println();
-		return index;
-	}
-
-	/**
-	 * check if it is divisible by 2 or 5
-	 * @param n
-	 * @return
-	 */
-	private static boolean check(int n) {
-		while (n % 2 == 0) {
-			n /= 2;
-		}
-		if (n == 1) {
-			return true;
-		} else {
-			while (n % 5 == 0) {
-				n /= 5;
-			}
-			if (n == 1) {
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private static int getDividerBit(int n) {
-		int i = 1;
-		while ((n /= 10) > 0) {
-			i++;
-		}
-		return i;
 	}
 
 }
